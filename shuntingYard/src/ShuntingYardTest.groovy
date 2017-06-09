92132ffffcbeee9bfeb80d7814b22757ab47a9ce
Benjamin Wijk <benjamin.wijk@gmail.com>
2017-06-09 11:27:26 +0200

    added more test cases

--- shuntingYard/src/ShuntingYardTest.groovy
+++ shuntingYard/src/ShuntingYardTest.groovy
@@ -5,7 +5,14 @@ class ShuntingYardTest extends groovy.util.GroovyTestCase {
 
     void testSortToRPN() {
         ShuntingYard sy = new ShuntingYard();
-        assertEquals("1 2 3 * 4 5 / 6 - 7 * - 8 * +",sy.sortToRPN("1 + ( 2 * 3 - ( 4 / 5 - 6 ) * 7 ) * 8"));
 
+        assertEquals("1 2 3 * 4 5 / 6 - 7 * - 8 * +",
+                sy.sortToRPN("1 + ( 2 * 3 - ( 4 / 5 - 6 ) * 7 ) * 8"));
+
+        assertEquals("3 4 2 * 1 5 - 2 3 ^ ^ / +",
+                sy.sortToRPN("3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3"));
+
+       assertEquals("2 3 max 3 / 3.1415 * sin",
+                sy.sortToRPN("sin( max( 2 , 3 ) / 3 * 3.1415 )"));
     }
 }
