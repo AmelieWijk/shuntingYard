052e384aef478df350c41b25a9628f55891ec85d
Benjamin Wijk <benjamin.wijk@gmail.com>
2017-06-12 15:38:04 +0200

    added simple formatter

---
+++ shuntingYard/src/formatter/Formatter.java
@@ -0,0 +1,26 @@
+package formatter;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by Benjamin Wijk on 2017-06-12.
+ */
+public class Formatter {
+
+    public static String formatString(String toFormat){
+        Pattern number = Pattern.compile("\\d+(\\.\\d+)?"); //allows integer or double.
+        Pattern operator = Pattern.compile("[-+*^/]");
+        Pattern function = Pattern.compile("[a-zA-Z]*\\(");
+        Pattern rightParenthesis = Pattern.compile("\\)");
+        Pattern separator = Pattern.compile(",");
+
+        toFormat = number.matcher(toFormat).replaceAll("$0 ");
+        toFormat = operator.matcher(toFormat).replaceAll("$0 ");
+        toFormat = function.matcher(toFormat).replaceAll("$0 ");
+        toFormat = rightParenthesis.matcher(toFormat).replaceAll("$0 ");
+        toFormat = separator.matcher(toFormat).replaceAll("$0 ");
+
+        return toFormat;
+    }
+
+}
