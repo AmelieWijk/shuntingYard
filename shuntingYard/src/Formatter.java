05252fa79e89e9bb0f4933c37e1d2445bf0c9621
Benjamin Wijk <benjamin.wijk@gmail.com>
2017-06-12 14:35:24 +0200

    added formatter

---
+++ shuntingYard/src/Formatter.java
@@ -0,0 +1,63 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Benjamin Wijk on 2017-06-12.
+ */
+public class Formatter {
+    List<Pattern> patterns;
+    StringJoiner sj;
+
+    public Formatter(String string){
+        setupPatterns();
+
+
+
+    }
+
+    public String formatString(String string) {
+        sj = new StringJoiner(" ");
+        Matcher match = null;
+
+        while(!string.isEmpty()){
+            for (Pattern p : patterns) {
+                match = p.matcher(string);
+                if (match.find()) {
+                    break;
+                }
+            }
+            sj.add(match.group());
+            string = string.substring(match.end());
+        }
+
+       return sj.toString();
+    }
+
+    public void setupPatterns(){
+        Pattern number = Pattern.compile("^-?\\d+(\\.\\d+)?"); //Optional "-" at start, allows integer or double.
+        Pattern operator = Pattern.compile("^[-+*^/]");
+        Pattern function = Pattern.compile("^[a-zA-Z]*\\(");
+        //Pattern leftParenth = Pattern.compile("^\\("); //Not needed, covered by function.
+        Pattern rightParenthesis = Pattern.compile("\\)");
+        Pattern separator = Pattern.compile(",");
+
+        patterns = new LinkedList<>();
+        patterns.add(number);
+        patterns.add(operator);
+        patterns.add(function);
+        patterns.add(rightParenthesis);
+        patterns.add(separator);
+    }
+
+    public static void main (String[] args){
+    Formatter form = new Formatter("321312-32.423(hej(3+3))");
+
+
+
+
+    }
+
+}
---
+++ shuntingYard/src/FormatterTest.groovy
@@ -0,0 +1,8 @@
+/**
+ * Created by Elev1 on 2017-06-12.
+ */
+class FormatterTest extends groovy.util.GroovyTestCase {
+
+
+
+}
