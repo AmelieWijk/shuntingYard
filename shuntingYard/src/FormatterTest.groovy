a112bdf7b71eed775bd97ef72af384b6c5bda932
Benjamin Wijk <benjamin.wijk@gmail.com>
2017-06-12 11:37:30 +0200

    added files

---
+++ shuntingYard/src/FormatterTest.groovy
@@ -0,0 +1,21 @@
+/**
+ * Created by Benjamin Wijk on 2017-06-12.
+ */
+class FormatterTest extends groovy.util.GroovyTestCase{
+
+    public testNumber(){
+
+    }
+
+    public testOperator(){
+
+    }
+
+    public testFunction(){
+
+    }
+
+
+
+
+}
---
+++ shuntingYard/src/tokens/Tokenizer.java
@@ -0,0 +1,7 @@
+package tokens;
+
+/**
+ * Created by Benjamin Wijk on 2017-06-12.
+ */
+public class Tokenizer {
+}
